ff30221f093925577270add2886283a75f3d3b95
Parmeet Singh <parmeet.asi@gmail.com>
2022-11-17 03:17:29 +0530

    add theme and landing page

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,24 +1,45 @@
-import { Box, Button, Container, Flex, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+} from "@chakra-ui/react";
 
 type Props = {
   children?: React.ReactNode;
+  showRequestTestimonial?: boolean;
 };
 
-const RootPageLayout = ({ children }: Props) => {
+const RootPageLayout = (
+  { children, showRequestTestimonial }: Props = {
+    showRequestTestimonial: false,
+  }
+) => {
   return (
-    <>
+    <Box bgColor="purple.100">
       <Box as="section" pb={{ base: "12", md: "24" }}>
-        <Box as="nav" bg="bg-surface" boxShadow="md">
+        <Box bgColor="whiteAlpha.500" as="nav" bg="bg-surface" boxShadow="md">
           <Container maxW="container.xl" py={{ base: "3", lg: "5" }}>
             <Flex alignItems="center" justify="space-between">
-              <Heading>GetReco</Heading>
-              <Button>Request Testimonial</Button>
+              <HStack>
+                <Image w="14" src="testimonial.svg" alt="" />
+                <Heading>
+                  Get
+                  <span className="text-purple-800">Reco</span>
+                </Heading>
+              </HStack>
+              {showRequestTestimonial && <Button>Request Testimonial</Button>}
             </Flex>
           </Container>
         </Box>
       </Box>
-      <Container maxW="container.xl">{children}</Container>
-    </>
+      <Container minH="calc(100vh - 96px)" maxW="container.xl">
+        {children}
+      </Container>
+    </Box>
   );
 };
 
--- src/pageContainers/LandingPage/LandingPage.tsx
+++ src/pageContainers/LandingPage/LandingPage.tsx
@@ -1,8 +1,18 @@
+import { Button, Heading, Image, VStack } from "@chakra-ui/react";
+
 const HomePageContainer = () => {
   return (
-    <div>
-      <h2>Home page container</h2>
-    </div>
+    <VStack textAlign="center" spacing="12">
+      <Image w="40" src="testimonial.svg" alt="" />
+      <Heading w="5xl" textAlign="center" size="3xl">
+        Get all your company{"'"}s
+        <span className="text-purple-800"> testimonials </span>
+        added at one place
+      </Heading>
+      <Button size="lg" colorScheme="purple">
+        Add your Company
+      </Button>
+    </VStack>
   );
 };
 
