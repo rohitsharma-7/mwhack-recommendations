970b830ec7ded82c9674eb8d1da24111b3bc4d31
Parmeet Singh <parmeet.asi@gmail.com>
2022-11-17 21:14:54 +0530

    working on testimonial card

---
+++ src/components/cards/TestimonialCard/TestimonialCard.tsx
@@ -0,0 +1,35 @@
+import { Avatar, Card, Flex, HStack, Text } from "@chakra-ui/react";
+
+type Props = {
+  testimonialData: {
+    name: string;
+    avatar: string;
+    message: string;
+    country: string;
+  };
+};
+
+const TestimonialCard = ({
+  testimonialData: { name, avatar, message, country },
+}: Props) => {
+  return (
+    <Card w="full" px="5" py="8" borderRadius="2xl" bgColor="white">
+      <HStack mb="2">
+        <Avatar src={avatar} name={name} size="md" />
+        <Flex direction="column" alignItems="flex-start">
+          <Text fontWeight="semibold">{name} </Text>
+          <Text fontSize="sm" color="purple.400">
+            {country}
+          </Text>
+        </Flex>
+      </HStack>
+      <Text fontStyle="italic" textAlign="left" fontSize="md">
+        {'"'}
+        {message}
+        {'"'}
+      </Text>
+    </Card>
+  );
+};
+
+export default TestimonialCard;
--- src/pageContainers/EndorsementListingPage/EndorsementListingPageContainer.tsx
+++ src/pageContainers/EndorsementListingPage/EndorsementListingPageContainer.tsx
@@ -1,5 +1,6 @@
-import { HStack } from "@chakra-ui/react";
+import { HStack, VStack } from "@chakra-ui/react";
 import { CompanyCard } from "../../components/cards";
+import TestimonialCard from "../../components/cards/TestimonialCard";
 
 const dummyCompanyData = {
   companyName: "Meltwater",
@@ -7,7 +8,7 @@ const dummyCompanyData = {
     "https://media-exp1.licdn.com/dms/image/C560BAQFqCEBmVvvSeA/company-logo_200_200/0/1656696462520?e=1676505600&v=beta&t=iPuGCGguq8YX1NUTgOAiQESMGriQCqiGSpWNyHfsfAI",
   description:
     "Leading media monitoring & social analytics platform helping businesses understand and influence the world around them.",
-  category: " IT Services and IT Consulting",
+  category: " IT Services",
   address: {
     city: "San Francisco",
     country: "USA",
@@ -15,11 +16,27 @@ const dummyCompanyData = {
   website: "https://www.meltwater.com/en",
 };
 
+const dummyTestimonialData = {
+  name: "Parmeet Singh",
+  avatar:
+    "https://media-exp1.licdn.com/dms/image/C4E03AQH3_Ax8RSDUzw/profile-displayphoto-shrink_800_800/0/1627945999467?e=1674086400&v=beta&t=2a7Nf2jQf3-fK5df5E2XyJr5diMvU8ZGKlO_j3FThUw",
+  message:
+    "Lorem Ipsum  Leading media monitoring & social analytics platform helping businesses understand and influence the world around them. Leading media monitoring & social analytics platform helping businesses understand and influence the world around them. Leading media monitoring & social analytics platform helping businesses understand and influence the world around them. Leading media monitoring & social analytics platform helping businesses understand and influence the world around them. Lorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+  country: "India",
+};
+
 const EndorsementListingPageContainer = () => {
   return (
-    <HStack alignItems="flex-start" spacing="8">
+    <HStack alignItems="flex-start" spacing="8" pb="24">
       <CompanyCard companyData={dummyCompanyData} />
-      <h2>Endorsement Listing Page Container</h2>
+      <HStack w="calc(100vw - 600px)">
+        <VStack w="full" spacing="4">
+          <TestimonialCard testimonialData={dummyTestimonialData} />
+          <TestimonialCard testimonialData={dummyTestimonialData} />
+          <TestimonialCard testimonialData={dummyTestimonialData} />
+          <TestimonialCard testimonialData={dummyTestimonialData} />
+        </VStack>
+      </HStack>
     </HStack>
   );
 };
