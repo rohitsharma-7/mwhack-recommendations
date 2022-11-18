d4321f4897353b5e465f5028b7b36340285c581c
Rohit Sharma <rohit.sharma@meltwater.com>
2022-11-18 18:02:50 +0530

    removed nextauth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,18 @@
 import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   // Create a client
   const queryClient = new QueryClient();
 
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </ChakraProvider>
-    </SessionProvider>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ChakraProvider>
   );
 };
 
